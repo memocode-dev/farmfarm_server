2052b9ad7d786d1ed3bfafe8b550282688b5e0d8
audgks5551 <audgsk5551@daum.net>
2024-06-24 16:31:47 +0900

    Update: dev/prod 환경에 나눠서 토픽이 정해질 수 있도록 수정

--- src/main/java/dev/memocode/farmfarm_server/mqtt/config/AwsIotMqtt5Service.java
+++ src/main/java/dev/memocode/farmfarm_server/mqtt/config/AwsIotMqtt5Service.java
@@ -2,6 +2,7 @@ package dev.memocode.farmfarm_server.mqtt.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.crt.mqtt5.Mqtt5Client;
 import software.amazon.awssdk.crt.mqtt5.QOS;
@@ -16,12 +17,16 @@ import java.util.concurrent.TimeoutException;
 @RequiredArgsConstructor
 public class AwsIotMqtt5Service {
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     public void start(Mqtt5Client mqtt5Client) throws ExecutionException, InterruptedException, TimeoutException {
         mqtt5Client.start();
 
         /* Subscribe */
+        String topicFilter = profile.equals("prod") ? "prod/response/+" : "dev/response/+";
         SubscribePacket.SubscribePacketBuilder subscribeBuilder = new SubscribePacket.SubscribePacketBuilder();
-        subscribeBuilder.withSubscription("response/+",
+        subscribeBuilder.withSubscription(topicFilter,
                 QOS.AT_LEAST_ONCE, false, false, SubscribePacket.RetainHandlingType.DONT_SEND);
         mqtt5Client.subscribe(subscribeBuilder.build()).get(60, TimeUnit.SECONDS);
     }
