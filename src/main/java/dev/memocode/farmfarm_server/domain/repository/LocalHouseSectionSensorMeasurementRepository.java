bd8d2496fbbe0ad51bcf684eb5562349fdbde675
audgks5551 <audgsk5551@daum.net>
2024-07-07 19:22:30 +0900

    Update: 센서 그래프가 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60분 단위로 딱 나눠 떨어지게 수정2

--- src/main/java/dev/memocode/farmfarm_server/domain/repository/LocalHouseSectionSensorMeasurementRepository.java
+++ src/main/java/dev/memocode/farmfarm_server/domain/repository/LocalHouseSectionSensorMeasurementRepository.java
@@ -19,7 +19,7 @@ public interface LocalHouseSectionSensorMeasurementRepository
     Optional<LocalHouseSectionSensorMeasurement> findTopByLocalHouseSectionSensorIdAndMeasurementTypeOrderByMeasuredAtDesc(
             UUID houseSectionSensorId, MeasurementType measurementType);
 
-    @Query(value = "SELECT date_trunc('minute', measured_at) - interval '1 minute' * (extract(minute from measured_at)::int % 5) AS interval_start, " +
+    @Query(value = "SELECT date_trunc('hour', m.measured_at) + interval '1 minute' * (floor(extract(minute from m.measured_at) / 10) * 10) AS interval_start, " +
             "m.measurement_type, " +
             "AVG(m.value) AS avg_value " +
             "FROM local_house_section_sensor_measurements m " +
